1244ecf9804d5b2ea7358675d9cc8cdf33900585
eamonnmcgurk <eamonnmcgurk@outlook.com>
2015-09-30 10:55:35 +0100

    TEST HOME PAGE ADAPTED

--- src/market/testing/TestHomePage.java
+++ src/market/testing/TestHomePage.java
@@ -1,5 +1,28 @@
 package market.testing;
 
-public class TestHomePage {
+import static org.junit.Assert.*;
+import market.dal.Feed;
 
-}
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestHomePage {
+	private feed feed;
+	
+	@Before
+	public void setUp() throws Exception{
+		feed = new Feed();
+	}
+	
+	@After 
+	public void tearDown()  throws Exception{
+		feed=null;
+	}
+	
+	@Test
+	public void testFeed(){
+		int result;
+		result = feed.mySquare(3);
+		assertEquals(9, result);
+	}
